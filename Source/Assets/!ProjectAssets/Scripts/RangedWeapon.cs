3e4141a5c33d10ddcab0a3e2deb7f04c2452afc8
Ryan <morrisman_3@hotmail.com>
2014-10-11 03:21:22 -0600

    Updated InputHandler, Added Weapon and RangedWeapon
    Added some "test" code for examples on delegates. Tried to explain this
    in class, but this might make more sense.

---
+++ Source/Assets/!ProjectAssets/Scripts/RangedWeapon.cs
@@ -0,0 +1,52 @@
+﻿/* RangedWeapon.cs
+*  Last Edited By:	Ryan Morris
+*					11 October 2014
+*
+* This is a class that inherits the Weapon class. I created this for testing delegates.
+*/
+
+using UnityEngine;
+
+public class RangedWeapon : Weapon {
+	// Variable Members
+	private float range;
+	private string animation;
+	
+	// Constructor
+	public RangedWeapon( string modelFile, string ani, float r )
+		: base( modelFile )
+	{
+		this.range = r;
+		this.animation = ani;
+	}
+	
+	// shoot a sphere projectile
+	public void sphere_shot( Transform shooter ) {
+		Transform proj = GameObject.CreatePrimitive(PrimitiveType.Sphere).GetComponent<Transform>();
+		proj.position = shooter.position + shooter.forward;
+		proj.rotation = shooter.rotation;
+		proj.Rotate(Vector3.up, (Random.value - .5f) * 7.5f);
+		proj.localScale = new Vector3(.2f, .2f, .2f);
+		proj.gameObject.AddComponent<BasicProjectile>();
+	}
+	
+	// shoot a cube projectile
+	public void cube_shot( Transform shooter ) {
+		Transform proj = GameObject.CreatePrimitive(PrimitiveType.Cube).GetComponent<Transform>();
+		proj.position = shooter.position + shooter.forward;
+		proj.rotation = shooter.rotation;
+		proj.Rotate(Vector3.up, (Random.value - .5f) * 7.5f);
+		proj.localScale = new Vector3(0.3f, 0.2f, 1.5f);
+		proj.gameObject.AddComponent<BasicProjectile>();
+	}
+	
+	// shoot a plane projectile
+	public void plane_shot( Transform shooter ) {
+		Transform proj = GameObject.CreatePrimitive(PrimitiveType.Plane).GetComponent<Transform>();
+		proj.position = shooter.position + shooter.forward;
+		proj.rotation = shooter.rotation;
+		proj.Rotate(Vector3.up, (Random.value - .5f) * 7.5f);
+		proj.localScale = new Vector3(.2f, .2f, .2f);
+		proj.gameObject.AddComponent<BasicProjectile>();
+	}
+}
---
+++ Source/Assets/!ProjectAssets/Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+﻿/* Weapon.cs
+*  Last Edited By:	Ryan Morris
+*					11 October 2014
+*
+* This is an abstract class I created for testing delegates.
+*/
+
+public abstract class Weapon {
+	// Variable Members
+	private string model;
+	private float damage, speed;
+	
+	// Constructor
+	public Weapon( string modelFile ) {
+		model = modelFile;
+		damage = 1;
+		speed = 1;
+	}
+}
