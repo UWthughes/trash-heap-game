d139749b94a3b14dceb350287c8dd7cfae6b4cf0
ademillard <ademillard@gmail.com>
2014-11-18 15:57:22 -0700

    enemy objects and healthbar

---
+++ Source/Assets/!ProjectAssets/Scripts/EnemyObject.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyObject : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public GameObject enemyInstance;
+    public GameObject healthBar;
+    public EnemyStats stats;
+    private GameObject healthBarInstance;
+    int count;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject o = Instantiate(prefabs[Random.Range(0, prefabs.Length)],
+                                           enemyInstance.transform.position,
+                                           Quaternion.identity) as GameObject;
+        o.transform.parent = enemyInstance.transform;
+
+        healthBarInstance = Instantiate(healthBar, transform.position, Quaternion.identity) as GameObject;
+
+        float hpPercent = (float)(stats.HealthPoints / 100.0);
+        float hpBarWidth = (hpPercent * 50);
+        healthBarInstance.guiTexture.pixelInset = new Rect(-25, 25, hpBarWidth, 5);
+
+        if (stats.HealthPoints < 33)
+            healthBarInstance.guiTexture.color = Color.red;
+        else if (stats.HealthPoints < 66)
+            healthBarInstance.guiTexture.color = Color.yellow;
+
+        healthBarInstance.transform.localScale = Vector3.zero;
+
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (stats.Agility < 25)
+            transform.Translate((count > 500) ? Vector3.left * Time.deltaTime : Vector3.right * Time.deltaTime);
+        else if (stats.Agility < 50)
+            transform.Translate((count > 500) ? Vector3.forward * Time.deltaTime : Vector3.back * Time.deltaTime);
+        else if (stats.Agility < 75)
+            transform.Translate((count > 500) ? Vector3.back * Time.deltaTime : Vector3.forward * Time.deltaTime);
+        else
+            transform.Translate((count > 500) ? Vector3.right * Time.deltaTime : Vector3.left * Time.deltaTime);
+
+        if (healthBarInstance != null)
+        {
+            healthBarInstance.transform.position = Camera.main.WorldToViewportPoint(transform.position);
+            float hpPercent = (float)(stats.HealthPoints / 100.0);
+            float hpBarWidth = (hpPercent * 50);
+            healthBarInstance.guiTexture.pixelInset = new Rect(-25, 25, hpBarWidth, 5);
+
+            if (stats.HealthPoints < 33)
+                healthBarInstance.guiTexture.color = Color.red;
+            else if (stats.HealthPoints < 66)
+                healthBarInstance.guiTexture.color = Color.yellow;
+            else
+                healthBarInstance.guiTexture.color = Color.green;
+        }
+
+        count++;
+        if (count > 1000)
+            count = 0;
+    }
+}
---
+++ Source/Assets/!ProjectAssets/Scripts/EnemyStats.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : MonoBehaviour {
+    public int HealthPoints;
+    public int AttackPower;
+    public int Strength;
+    public int Intellect;
+    public int Agility;
+
+	// Use this for initialization
+	void Start () {
+        HealthPoints = Random.Range(0, 100);
+        AttackPower = Random.Range(0, 100);
+        Strength = Random.Range(0, 100);
+        Intellect = Random.Range(0, 100);
+        Agility = Random.Range(0, 100);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (HealthPoints < 0)
+            HealthPoints = 0;
+        else if (HealthPoints > 100)
+            HealthPoints = 100;
+	}
+}
