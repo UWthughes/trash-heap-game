841ac17d577379475b47ea521672c25ae4684748
Ryan <morrisman_3@hotmail.com>
2015-04-27 21:57:29 -0600

    Simple Bunny Movement, Not A*
    Added simple movement to one enemy. Sort of a test before trying
    anything harder, such as path finding with A*...

--- Source/Assets/UnitySurvStuff/_CompletedAssets/Scripts/Enemy/EnemyMovement.cs
+++ Source/Assets/UnitySurvStuff/_CompletedAssets/Scripts/Enemy/EnemyMovement.cs
@@ -5,36 +5,66 @@ namespace CompleteProject
 {
     public class EnemyMovement : MonoBehaviour
     {
-        Transform player;               // Reference to the player's position.
-       // PlayerHealth playerHealth;      // Reference to the player's health.
-        EnemyHealth enemyHealth;        // Reference to this enemy's health.
-        //NavMeshAgent nav;               // Reference to the nav mesh agent.
+        GameObject[] players;             // all players in the game
+		Transform target;				  // the closest player
+		float targetDistance;
+		int moveSpeed = 3;
+		int rotationSpeed = 3;
+		float range1 = 10f;
+		float range2 = 10f;
+		float stop = 0f;
+        //PlayerHealth playerHealth;      // Reference to the player's health.
+        //EnemyHealth enemyHealth;        // Reference to this enemy's health.
 
 
         void Awake ()
         {
             // Set up the references.
-            player = GameObject.FindGameObjectWithTag ("Player").transform;
-           // playerHealth = player.GetComponent <PlayerHealth> ();
-            enemyHealth = GetComponent <EnemyHealth> ();
-            //nav = GetComponent <NavMeshAgent> ();
+            players = GameObject.FindGameObjectsWithTag("Player");
+			InvokeRepeating("AcquireTarget", 0, 3);
+            // playerHealth = player.GetComponent <PlayerHealth> ();
+            //enemyHealth = GetComponent <EnemyHealth> ();
         }
 
 
         void Update ()
         {
-            // If the enemy and the player have health left...
-            if(enemyHealth.currentHealth > 0 )//&& playerHealth.currentHealth > 0)
-            {
-                // ... set the destination of the nav mesh agent to the player.
-                //nav.SetDestination (player.position);
-            }
-            // Otherwise...
-            else
-            {
-                // ... disable the nav mesh agent.
-                //nav.enabled = false;
-            }
+			if( target != null ) {
+				float distance = Vector3.Distance(transform.position, target.position);
+				// rotate to look at the player
+				Vector3 tempVec = new Vector3(target.position.x, transform.position.y, target.position.z);
+				if( distance <= range2 && distance >= range1 ) {
+					transform.rotation = Quaternion.Slerp(transform.rotation,
+					                                      Quaternion.LookRotation(tempVec - transform.position), rotationSpeed * Time.deltaTime);
+				}
+				// move towards the player
+				else if( distance <= range1 && distance > stop ) {
+					transform.rotation = Quaternion.Slerp(transform.rotation,
+					                                      Quaternion.LookRotation(tempVec - transform.position), rotationSpeed * Time.deltaTime);
+					                                      //Quaternion.LookRotation(target.position - transform.position), rotationSpeed * Time.deltaTime);
+					transform.position += transform.forward * moveSpeed * Time.deltaTime;
+				}
+				// just turn to player
+				else if( distance <= stop ) {
+					transform.rotation = Quaternion.Slerp(transform.rotation,
+					                                      Quaternion.LookRotation(tempVec - transform.position), rotationSpeed * Time.deltaTime);
+					                                      //Quaternion.LookRotation(target.position - transform.position), rotationSpeed * Time.deltaTime);
+				}
+			}
         }
+
+		void AcquireTarget() { 
+			if( players.Length > 0 ) { 
+				target = players[0].transform;
+				float closestDistance = Vector3.Distance(transform.position, target.position);
+				for( int i = 1; i < players.Length; i++ ) {
+					float distance = Vector3.Distance(transform.position, players[i].transform.position);
+					if( distance < closestDistance ) {
+						closestDistance = distance;
+						target = players[i].transform;
+					}
+				}
+			}
+		}
     }
 }
